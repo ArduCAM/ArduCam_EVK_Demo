19986bdf54622c44d18caa461bb27b6336e0b898
antonio-ardu <Antonio@arducam.com>
2024-06-12 15:59:01 +0800

    Update utils.cpp

--- c/COption.cmake
+++ c/COption.cmake
@@ -9,9 +9,11 @@ if (NOT DEFINED C_CMAKE_OPTION)
 
     if(CMAKE_COMPILER_IS_GNUCXX)
         set(CMAKE_CXX_STANDARD 17)
+        set(CMAKE_C_STANDARD 17)
         message(STATUS "optional: C++17")
     else(CMAKE_COMPILER_IS_GNUCXX)
         set(CMAKE_CXX_STANDARD 20)
+        set(CMAKE_C_STANDARD 17)
         message(STATUS "optional: C++20")
     endif(CMAKE_COMPILER_IS_GNUCXX)
 
@@ -32,6 +34,7 @@ if (NOT DEFINED C_CMAKE_OPTION)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -fPIC")
 
     if(NOT DEFINED arducam_evk_sdk_FOUND)
+        # TODO: download arducam_evk_sdk package when windows platform
         find_package(arducam_evk_sdk)
         message(STATUS "arducam_evk_sdk version: ${arducam_evk_sdk_VERSION}")
         if(WIN32)
